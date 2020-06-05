4f8ce48c48a6910ca7b578d945a6524c09388700
Camden Foucht <foucht@gmail.com>
2020-06-04 17:19:04 -0700

    Created basic chat input and connected it with the chat provider. Made small UI fixes.

--- client/src/components/Chat/MessageList.tsx
+++ client/src/components/Chat/MessageList.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import Message from "./Message";
 import { useChatMessages } from "../../providers/MessagingProvider";
 
+// The author and timestamp are hardcoded in. Eventually need to modify
+// useChatMessages to give an array of objects with text, author, and timestamp.
+
 const MessageList = () => {
   const chatMessages = useChatMessages();
 
-  const messages = chatMessages.map((el, index) => (
+  const messages = chatMessages.map((el: string, index: number) => (
     <Message
       key={index}
       text={el}
